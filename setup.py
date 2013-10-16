bb740a9020d1009594fe9a5b143dc77c49ce4a39
jdhughes <jdhughes@usgs.gov>
2013-10-16 12:38:17 -0400

    Added setup.py to facilitate installation of cluster_repo from git repository

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+# To use:
+#	   python setup.py bdist --format=wininst
+
+setup(name='flopy',
+      version='1.37',
+      author="Jeremy T. White and Joseph D. Hughes",
+      author_email="jwhite@usgs.gov or jdhughes@usgs.gov",
+      scripts=['MFArrayUtil.py','MFBinaryClass.py','MFData.py','MFInterpolators.py','pestUtil.py','shapefile.py'],
+      install_requires = ['numpy>=1.7','pandas>=0.12'],
+      url = 'https://github.com/jtwhite79/cluster_repo.git',
+      description = 'python utilities for working with MODFLOW-based input and output data',
+      license = 'PSF',
+      keywords = ['MODFLOW','MODFLOW binary reader','MODFLOW array utility','shapefile library','PEST python utility'],
+     )
+
