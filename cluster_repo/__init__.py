1891c95b732dfedd0ab93ae1d914e868908561b6
jdhughes <jdhughes@usgs.gov>
2013-10-20 13:11:25 -0400

    Converted cluster_repo to a package

---
+++ cluster_repo/__init__.py
@@ -0,0 +1,7 @@
+#__init for cluster_repo Package
+from MFArrayUtil import *
+from MFBinaryClass import *
+from MFData import *
+from MFInterpolators import *
+from pestUtil import *
+from shapefile import *
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
-# To use:
-#	   python setup.py bdist --format=wininst
+#To use:
+# python setup.py install
 
-setup(name='cluster_repo',
-      version='1.37',
-      author="Jeremy T. White and Joseph D. Hughes",
-      author_email="jwhite@usgs.gov or jdhughes@usgs.gov",
-      scripts=['MFArrayUtil.py','MFBinaryClass.py','MFData.py','MFInterpolators.py','pestUtil.py','shapefile.py'],
-      install_requires = ['numpy>=1.7','pandas>=0.12'],
-      url = 'https://github.com/jtwhite79/cluster_repo.git',
-      description = 'python utilities for working with MODFLOW-based input and output data',
-      license = 'PSF',
-      keywords = ['MODFLOW','MODFLOW binary reader','MODFLOW array utility','shapefile library','PEST python utility'],
-     )
 
+setup(
+    name='cluster_repo',
+    version='1.37',
+    packages=['cluster_repo'],
+    install_requires = ['numpy>=1.7','pandas>=0.12'],
+    url='https://github.com/jtwhite79/cluster_repo.git',
+    license='PSF',
+    author='Jeremy T. White and  Joseph D. Hughes',
+    author_email='jwhite@usgs.gov or jdhughes@usgs.gov',
+    description='python utilities for working with MODFLOW-based input and output data'
+)
